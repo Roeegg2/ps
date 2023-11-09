5f3255d716407d50fe007176265c0151d5507603
roee toledano <roeet@roeetpc.lan>
2023-11-09 11:49:44 -0500

    got a basic version somewhat working. still need to take care of the rest of the values which dont get printed, and the formatting issue with spaced process names

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
 import os
-import sys
-import time
 
+title = ['PID', 'Comm', 'State', 'PPID', 'Terminal', 'Priority', 'Nice', 'Threads', 'Wchan', 'CPU', 'Time']
+#in order: PID, Comm, State, PPID, Controlling termianl, Priority, Nice, Num of threads, Wchan. missing CPU and time 
+indices = [0, 1, 2, 3, 6, 17, 18, 19, 34]
 
+def get_file_values(pid, filename):
+    values = []
+    file = open('/proc/' + pid + filename)
+    try:
+        i = 0
+        line = file.readline()
+        words = line.split()
+        for word in words:
+            if (i in indices):
+                values.append(word)
+            i += 1
 
-status_entries = ['Name:', 'State:']
-
-def func(pid, filename):
-    with open('/proc/' + pid + filename) as file: 
-        for line in file:
-                 
-
-def get_entry_value(line):
-    words = line.split()
-    if words[0] in status_entries:
-        return ''.join(words[1:])
+    except:
+        print("Error: unable to read process: {pid}'s file")
+    finally:
+        file.close()
+    
+    return values
 
 def main():
-    entries = os.listdir('/proc')
-    for entry in entries:
-        if (entry.isdigit()):
-            func(entry, '/status')
-
+    objects = os.listdir('/proc')
+    print("{:<6} {:<37} {:<6} {:<6} {:<8} {:<8} {:<10} {:<10} {:<10} {:<10} {:<10}".format(*title))
+    for object in objects:
+        if (object.isdigit()):
+            values = get_file_values(object, '/stat')
+            # print(values)
+            print("{:<6} {:<37} {:<6} {:<6} {:<8} {:<8} {:<10} {:<10} {:<10}".format(*values))
+            # {:<10} {:<10}
 
 main()
